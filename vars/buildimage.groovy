e3efb65942c092162968b55973a55f291cf42d79
root <root@LAPTOP-IH0EODNO.localdomain>
2023-09-01 18:19:23 -0400

    second

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -1,9 +1,11 @@
 def call() {
     ech0 "Building docker image"           
-    withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]){
-        sh "docker build -t docker-hub-id/myapp:${IMAGE_NAME} ."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-id', usernameVariable: 'USER', passwordVariable: 'PASS')]){
+        sh "docker build -t akodevops/myapp:${IMAGE_NAME} ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push docker-hub-id/myapp:${IMAGE_NAME}"
+        sh "docker push akodevops/myapp:${IMAGE_NAME}"
+               
     }
-            
+
 }
+            
