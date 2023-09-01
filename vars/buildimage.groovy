e927dea6ef8727898d3525afb56cdb1c764989c6
devopspanther <116690112+devopspanther@users.noreply.github.com>
2023-09-01 18:53:24 -0400

    Update buildimage.groovy

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -1,5 +1,5 @@
 def call() {
-    ech0 "Building docker image"           
+    echo "Building docker image"           
     withCredentials([usernamePassword(credentialsId: 'dockerhub-id', usernameVariable: 'USER', passwordVariable: 'PASS')]){
         sh "docker build -t akodevops/myapp:${IMAGE_NAME} ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
